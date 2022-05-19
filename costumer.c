1264f0fcffa54c22166c79535de232015adfb3ff
Marcoantonio Manso <83654329+marcomansomm@users.noreply.github.com>
2022-05-18 23:52:21 -0300

    update

--- costumer.c
+++ costumer.c
@@ -1,69 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthreads.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-typedef struct node{
-    char name[20];
-    float valor;
-    int quantidade;
-    struct node *next;
-}Node;
+int qtd_buffer = 0;
+int *buffer;
+int limite;
 
-typedef struct list{
-    int size;
-    Node *head;
-}List;
+sem_t mutex;
 
-Node* createList(){
-
-}
-
-int isEmpty (struct node *head){
-	if(head == NULL){
-		return 1;
-	} else {
-		return 0;
-	}
-
-}
-
-void func_produtor(void *arg);
-void func_consumidor(void *arg);
+void *func_produtor(void *arg);
+void *func_consumidor(void *arg);
 
 int main(void){
 	int qtd_consumidor=0, qtd_produtor=0;
-	printf("Quantos consumidores vão existir?> ");
 	scanf("%d", &qtd_consumidor);
-	printf("Quantos produtores vão existir?> ");
 	scanf("%d", &qtd_produtor);
+	scanf("%d", &limite);
+	scanf("%d", &qtd_buffer);
 
-	thread_t consumidores[qtd_consumidor], produtores[qtd_produtor];
+	buffer = malloc(qtd_buffer * sizeof(int));
+
+
+	sem_init(&mutex, 0, 1);
+
+	pthread_t consumidores[qtd_consumidor], produtores[qtd_produtor];
+
+	int produtor_id[qtd_produtor], consumidor_id[qtd_consumidor];
 
 	for(int i = 0; i<qtd_consumidor; i++){
-		thread_create(&(consumidor[i]), NULL, func_consumidor, NULL);
+		pthread_create(&(consumidores[i]), NULL, func_consumidor, &consumidor_id[i]);
 	}
 
 	for(int i = 0; i<qtd_produtor; i++){
-		thread_create(&(produtor[i]), NULL, func_produtor, NULL);
+		pthread_create(&(produtores[i]), NULL, func_produtor, &produtor_id[i]);
 	}
 
 	for(int i = 0; i<qtd_consumidor; i++){
-		thread_join(consumidores[i], NULL);
+		pthread_join(consumidores[i], NULL);
 	}
 
 	for(int i = 0; i<qtd_consumidor; i++){
-		thread_join(produtores[i], NULL);
+		pthread_join(produtores[i], NULL);
 	}
 
 	return 0;
 }
 
-void func_consumidor(void *arg){
+void *func_consumidor(void *id){
+	int consumido = 0, j = 0;
+	int *id_consumidor = (int*) id;
+	// pegar um valor do buffer
+	while(1){ // loop infinito para percorrer o buffer
+		
+		if(buffer[j] == 0){
+			sem_wait(&mutex);
+			consumido = buffer[j]; // ele vai pegar o numero
+			printf("Produtor %d produzindo %d na posição %d\n", *id_consumidor, consumido, j);
+			buffer[j] = 0; // e liberar o espaço no buffer
+			sem_post(&mutex);
+		}
 
-	return arg;
+		if(j == limite){
+			j = 0;
+		}
+
+		j++;
+		
+	}
 }
 
-void func_produtor(void *arg){
-	
-	return arg;
+
+
+void *func_produtor(void *id){
+
+	int *id_produtor = (int*) id;
+	int produto, i=0, j=0;
+	while(1){
+		if(i >= limite){
+			i = 0;
+		} else {
+			produto = (2 * i) + 1;	
+		}
+		
+		while(1){
+			if(buffer[j] == 0){
+				sem_wait(&mutex);
+				buffer[j] = produto;
+				printf("Produtor %d produzindo %d na posição %d\n", *id_produtor, produto, j);
+				sem_post(&mutex);
+				break;
+			}
+			
+			if (j == qtd_buffer){
+				j = 0;
+			}
+			
+			j++;
+		}
+
+		i++;
+	}
 }
